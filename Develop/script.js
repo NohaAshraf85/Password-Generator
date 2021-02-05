ece6466b8f123d307f643e7afb41f7b66971d118
NohaAshraf <noha_ashraf85@hotmail.com>
2021-02-04 20:25:30 -0500

    created the variables for the password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,12 @@
 // Assignment Code
+var passwordLength = prompt ("Password length, pick a number from 8 to 128")
+var upperCaseCharacter = prompt ("Do you want uppercase characters?")
+var lowerCaseCharacter = prompt ("Do you want lowercase characters?")
+var numeric = prompt ("Do you want a number in your password?")
+var specialCharacters = prompt ("Do you want a special character in your password?")
+
+
+
 var generateBtn = document.querySelector("#generate");
 console.log(generate)
 
@@ -10,6 +18,13 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// function generatePassword()
+// {
+//   var characters=[" !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~","abcdefghijklmnopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUVWXYZ","1234567890"];
+  
+//   return "hello";
+// }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
